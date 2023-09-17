86aa5007ead8f83151e671c811d46173c75fb2ab
Ivan Buncic <ivan@triplsi.com>
2023-09-17 16:35:45 +0200

    Update Modal.tsx

--- islands/Modal.tsx
+++ islands/Modal.tsx
@@ -15,7 +15,7 @@ export default function Modal({ children, onClose }: ModalProps) {
   return (
     <div class={tw`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center justify-items-center content-center`} onClick={onClose}>
       <div class={tw`bg-white rounded-lg pt-8 relative w-screen h-screen overflow-hidden transition-all items-center justify-center`} onClick={stopPropagation}>
-        <button onClick={onClose} class={tw`absolute top-4 right-4 text-2xl font-bold m-2`}>X</button>
+        <button onClick={onClose} class={tw`absolute bg-purple-500 p-2 top-4 right-4 font-bold m-2 rounded font-semibold text-base`}>X</button>
         {children}
       </div>
     </div>
