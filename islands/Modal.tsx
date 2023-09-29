263f1cd2ded98d0c1cce6f07a75e18f296c565fb
Ivan Buncic <ivan@triplsi.com>
2023-09-29 20:49:42 +0200

    esc

--- islands/Modal.tsx
+++ islands/Modal.tsx
@@ -1,23 +1,50 @@
 /** @jsx h */
 import { h } from 'preact';
+import { useEffect } from 'preact/hooks';
 import { tw } from 'twind';
 
 interface ModalProps {
-  children: preact.ComponentChild;
-  onClose: () => void;
+    children: preact.ComponentChild;
+    onClose: () => void;
 }
 
 export default function Modal({ children, onClose }: ModalProps) {
-  const stopPropagation = (e: Event) => {
-    e.stopPropagation();
-  };
+    const stopPropagation = (e: Event) => {
+        e.stopPropagation();
+    };
 
-  return (
-    <div class={tw`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center justify-items-center content-center`} onClick={onClose}>
-      <div class={tw`bg-white rounded-lg pt-8 relative w-screen h-screen overflow-hidden transition-all items-center justify-center`} onClick={stopPropagation}>
-        <button onClick={onClose} class={tw`absolute bg-purple-500 text-white p-2 top-4 right-4 font-bold m-2 rounded font-semibold text-base`}>x</button>
-        {children}
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        const handleEscape = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                onClose();
+            }
+        };
+
+        self.addEventListener('keydown', handleEscape);
+
+        // Cleanup
+        return () => {
+            self.removeEventListener('keydown', handleEscape);
+        };
+    }, [onClose]);
+
+    return (
+        <div
+            class={tw`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center justify-items-center content-center`}
+            onClick={onClose}
+        >
+            <div
+                class={tw`bg-white rounded-lg pt-8 relative w-screen h-screen overflow-hidden transition-all items-center justify-center`}
+                onClick={stopPropagation}
+            >
+                <button
+                    onClick={onClose}
+                    class={tw`absolute bg-purple-500 text-white p-2 top-4 right-4 font-bold m-2 rounded font-semibold text-base`}
+                >
+                    x
+                </button>
+                {children}
+            </div>
+        </div>
+    );
 }
