5b64394a218e8f8ebbe43ce5bc01e1133e26c222
Ivan Buncic <ivan@triplsi.com>
2024-06-20 23:26:58 +0200

    some tw stuck

--- islands/Fighters.tsx
+++ islands/Fighters.tsx
@@ -85,7 +85,7 @@ export default function Fighters() {
                         <button
                             onClick={fight}
                             disabled={isFightButtonClicked}
-                            class={tw`bg-purple-500 text-white py-2 px-4 rounded text-lg m-2 font-semibold text-base ${
+                            class={`bg-purple-500 text-white py-2 px-4 rounded text-lg m-2 font-semibold text-base ${
                                 isFightButtonClicked
                                     ? 'opacity-50 cursor-not-allowed'
                                     : ''
