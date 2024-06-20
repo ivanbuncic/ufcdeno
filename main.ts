43601fd7c4e36fd60099ad6ecbbfa6f582d26b57
Ivan Buncic <ivan@triplsi.com>
2024-06-21 00:29:25 +0200

    update to Fresh 1.6

--- main.ts
+++ main.ts
@@ -6,5 +6,6 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
 
-await start(manifest);
+await start(manifest, config);
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,8 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>ufcdeno</title>
+        <title>UFC Deno</title>
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
