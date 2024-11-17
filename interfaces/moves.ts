7551af60840ec25d74f912a914251b8281f5653b
Ivan Buncic <ivan@triplsi.com>
2024-11-17 18:16:33 +0100

    Update moves.ts

--- interfaces/moves.ts
+++ interfaces/moves.ts
@@ -1,7 +1,7 @@
 export enum MoveType {
-  /* 1  */ BLOCKED_STRIKING = "blocked poking",
-  /* 2  */ CALF_KICK = "calf kick hard",
-  /* 3  */ CROSS = "cross in the face",
+  /* 1  */ BLOCKED_STRIKING = "blocked strike",
+  /* 2  */ CALF_KICK = "calf kick",
+  /* 3  */ CROSS = "cross to the face",
   /* 4  */ DEFENDED_TAKE_DOWN = "defended a take-down",
   /* 5  */ DEFENDED_GRAPPLING = "defended scratching",
   /* 6  */ EYE_POKE = "eye poke",
@@ -13,13 +13,14 @@ export enum MoveType {
   /* 12 */ HOOK = "hook to the dome",
   /* 13 */ ILLEGAL_KNEE = "illegal knee",
   /* 14 */ JAB = "jab to the nose",
-  /* 15 */ KIMURA = "kimura attempt",
-  /* 16 */ REAR_NAKED_CHOKE = "rear naked choke attempt",
+  /* 15 */ KIMURA = "kimura",
+  /* 16 */ REAR_NAKED_CHOKE = "rear naked choke",
   /* 17 */ SLICING_ELBOW = "slicing elbow",
   /* 18 */ SPINNING_BACK_FIST = "spinning back fist",
   /* 19 */ STANDING_GUILLOTINE_CHOKE = "standing guillotine choke",
   /* 20 */ TAKE_DOWN = "take-down",
   /* 21 */ UPPERCUT = "uppercut to the chin",
+  /* 22 */ SPINNING_BACK_KICK = "spinning back kick"
 }
 
 export interface Move {
